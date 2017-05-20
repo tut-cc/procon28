62d44adfec849a7bc872ac9b90c3105b68b560ff
utdotk <utdotk@gmail.com>
2017-05-20 16:43:02 +0900

    into im

--- lib/imagawa/imagawa.hpp
+++ lib/imagawa/imagawa.hpp
@@ -3,41 +3,41 @@
 #include <utility>
 #include <vector>
 
-class Point {
-public:
-  Point();
-  Point(int x, int y);
-
-  int x, y;
-};
-
-class Piece {
-public:
-  Piece();
-  Piece(int id, const std::vector<Point> &vertexes,
-    const std::vector<double> &edges, const std::vector<double> &degs);
-
-  int id;
-  std::vector<Point> vertexes;
-
-  std::vector<double> edges;
-  std::vector<double> degs;
-};
-
-class Answer {
-  Answer();
-  Answer(int id, const std::vector<Point> &vertexes);
-
-  int id;
-  std::vector<Point> vertexes;
-};
-
 namespace cv {
   class Mat;
   class Vec4i;
 }
 
 namespace im {
+  class Point {
+  public:
+    Point();
+    Point(int x, int y);
+
+    int x, y;
+  };
+
+  class Piece {
+  public:
+    Piece();
+    Piece(int id, const std::vector<Point> &vertexes,
+      const std::vector<double> &edges, const std::vector<double> &degs);
+
+    int id;
+    std::vector<Point> vertexes;
+
+    std::vector<double> edges;
+    std::vector<double> degs;
+  };
+
+  class Answer {
+    Answer();
+    Answer(int id, const std::vector<Point> &vertexes);
+
+    int id;
+    std::vector<Point> vertexes;
+  };
+
   void hello();
 
   std::vector<cv::Vec4i> detectSegments(const cv::Mat &binaryImg);
