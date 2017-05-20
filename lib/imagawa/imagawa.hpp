f7da84ffaffc01655c8fe2159c02d34c4f0545ba
utdotk <utdotk@gmail.com>
2017-05-20 16:38:15 +0900

    Pieceクラス、Answerクラス定義

--- lib/imagawa/imagawa.hpp
+++ lib/imagawa/imagawa.hpp
@@ -11,6 +11,27 @@ public:
   int x, y;
 };
 
+class Piece {
+public:
+  Piece();
+  Piece(int id, const std::vector<Point> &vertexes,
+    const std::vector<double> &edges, const std::vector<double> &degs);
+
+  int id;
+  std::vector<Point> vertexes;
+
+  std::vector<double> edges;
+  std::vector<double> degs;
+};
+
+class Answer {
+  Answer();
+  Answer(int id, const std::vector<Point> &vertexes);
+
+  int id;
+  std::vector<Point> vertexes;
+};
+
 namespace cv {
   class Mat;
   class Vec4i;
