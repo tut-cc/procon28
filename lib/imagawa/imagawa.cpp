4bc398d533bd41a8dc5437cb0d8a8d6d054e2fd4
utdotk <utdotk@gmail.com>
2017-05-20 15:46:11 +0900

    moukoredeeeyaro

--- lib/imagawa/imagawa.cpp
+++ lib/imagawa/imagawa.cpp
@@ -6,18 +6,18 @@
 
 #include <iostream>
 
-im::Point::Point() : Point(0, 0) {}
+Point::Point() : Point(0, 0) {}
 
-im::Point::Point(int xx, int yy) : x(xx), y(yy) {}
+Point::Point(int xx, int yy) : x(xx), y(yy) {}
 
 void im::hello() {
   std::cout << "hello 今川" << std::endl;
 }
 
-std::vector<std::pair<im::Point, im::Point>> im::detectSegments(const cv::Mat &img) {
+std::vector<cv::Vec4i> im::detectSegments(const cv::Mat &binaryImg) {
 
 }
 
-std::vector<im::Point> im::detectVertexes(const std::vector<std::pair<Point, Point>> &segments) {
+std::vector<Point> im::detectVertexes(const std::vector<cv::Vec4i> &segments) {
 
 }
--- lib/imagawa/imagawa.hpp
+++ lib/imagawa/imagawa.hpp
@@ -3,21 +3,22 @@
 #include <utility>
 #include <vector>
 
+class Point {
+public:
+  Point();
+  Point(int x, int y);
+
+  int x, y;
+};
+
 namespace cv {
   class Mat;
+  class Vec4i;
 }
 
 namespace im {
-  class Point {
-  public:
-    Point();
-    Point(int x, int y);
-
-    int x, y;
-  };
-
   void hello();
 
-  std::vector<std::pair<Point, Point>> detectSegments(const cv::Mat &img);
-  std::vector<Point> detectVertexes(const std::vector<std::pair<Point, Point>> &segments);
+  std::vector<cv::Vec4i> detectSegments(const cv::Mat &binaryImg);
+  std::vector<Point> detectVertexes(const std::vector<cv::Vec4i> &segments);
 }
