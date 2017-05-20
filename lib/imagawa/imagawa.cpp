be938acfa9e85dd221315ea7f0f753e7390b6597
utdotk <utdotk@gmail.com>
2017-05-20 14:45:28 +0900

    エセ関数宣言

--- lib/imagawa/imagawa.cpp
+++ lib/imagawa/imagawa.cpp
@@ -4,3 +4,11 @@
 void im::hello() {
   std::cout << "hello 今川" << std::endl;
 }
+
+std::vector<im::Segment> im::detectSegments() {
+
+}
+
+std::vector<im::Point> im::detectVertexes() {
+
+}
--- lib/imagawa/imagawa.hpp
+++ lib/imagawa/imagawa.hpp
@@ -1,5 +1,13 @@
 #pragma once
 
+#include <vector>
+
 namespace im {
+  class Point {};
+  class Segment {};
+
   void hello();
+
+  std::vector<Segment> detectSegments();
+  std::vector<Point> detectVertexes();
 }
