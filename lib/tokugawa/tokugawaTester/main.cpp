60be20dcc71c1ac6c14f09e57aa38e5733b11749
Shingo Ohtsuka <capriciouswalk@gmail.com>
2017-09-22 13:44:23 +0900

    clipperのサンプルを動かす（未遂）

--- lib/tokugawa/tokugawaTester/main.cpp
+++ lib/tokugawa/tokugawaTester/main.cpp
@@ -1,25 +1,40 @@
 #include "tokugawa.hpp"
 #include "polyclipping/clipper.hpp"
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+
+#include <vector>
+
 using namespace ClipperLib;
 
-void testclipper(){
+void DrawPolygons(const Paths& paths, unsigned int fill_color, unsigned int line_color)
+{
+  cv::Mat img;
+  std::vector<cv::Point> points;
+  for (const Path& p : paths) {
+    
+  }
+}
 
+void testclipper()
+{
 	Paths subj(2), clip(1), solution;
 
 	//define outer blue 'subject' polygon
 	subj[0] << 
-	IntPoint(180,200) << IntPoint(260,200) <<
-	IntPoint(260,150) << IntPoint(180,150);
+	  IntPoint(180,200) << IntPoint(260,200) <<
+	  IntPoint(260,150) << IntPoint(180,150);
 
 	//define subject's inner triangular 'hole' (with reverse orientation)
 	subj[1] << 
-	IntPoint(215,160) << IntPoint(230,190) << IntPoint(200,190);
+	  IntPoint(215,160) << IntPoint(230,190) << IntPoint(200,190);
 
 	//define orange 'clipping' polygon
 	clip[0] << 
-	IntPoint(190,210) << IntPoint(240,210) << 
-	IntPoint(240,130) << IntPoint(190,130);
+	  IntPoint(190,210) << IntPoint(240,210) << 
+	  IntPoint(240,130) << IntPoint(190,130);
 
 	//draw input polygons with user-defined routine ... 
 	DrawPolygons(subj, 0x160000FF, 0x600000FF); //blue
@@ -33,7 +48,6 @@ void testclipper(){
 
 	//draw solution with user-defined routine ... 
 	DrawPolygons(solution, 0x3000FF00, 0xFF006600); //solution shaded green
-
 }
 
 int main()
