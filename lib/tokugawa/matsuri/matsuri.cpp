8f251579ceeda463c5fed7a030ba6ede39e9aaa6
Ohtsuka Shingo <capriciouswalk@gmail.com>
2017-10-03 19:25:16 +0900

    探索コードプロトタイプ
    てんでだめ。遅いし、ヒューリスティックスコアも良くなさそう

--- lib/tokugawa/matsuri/matsuri.cpp
+++ lib/tokugawa/matsuri/matsuri.cpp
@@ -10,16 +10,21 @@ const double PI = 4 * std::atan(1);
 
 namespace cl = ClipperLib;
 
-struct State {
-  cl::Paths waku;
-  cl::Paths uni;
+struct Info {
   std::bitset<MAX_NUM_OF_PEICES> set;
   std::vector<im::Point> haiti;
 
-  State(const cl::Paths& waku, const cl::Paths& uni = cl::Paths()) : set(), haiti(MAX_NUM_OF_PEICES) {
-    this->waku = waku;
-    this->uni = uni;
-  }
+  Info() : set(), haiti(MAX_NUM_OF_PEICES, im::Point(-1, -1)) {}
+  Info(const std::bitset<MAX_NUM_OF_PEICES>& set, const std::vector<im::Point>& haiti) : set(set), haiti(haiti) {}
+};
+
+struct State {
+  cl::Paths waku;
+  cl::Paths uni;
+  Info info;
+
+  State(const cl::Paths& waku, const cl::Paths& uni = cl::Paths(), const Info& info = Info())
+      : waku(waku), uni(uni), info(info) {}
 };
 
 static cl::Paths piece2paths(const im::Piece& piece)
@@ -113,7 +118,7 @@ public:
 
 #include <iostream>
 #include <string>
-void test_degree()
+static void test_degree()
 {
   cl::Paths test(1);
   test[0] << cl::IntPoint(0, 0) << cl::IntPoint(50, 100) << cl::IntPoint(100, 0);
@@ -137,6 +142,132 @@ void test_degree()
   }
 }
 
+static std::pair<bool, cl::Paths> cut_waku(const cl::Paths& waku, const cl::Path& path)
+{
+  // waku から path を削り取る
+  cl::Clipper clipper;
+  clipper.AddPaths(waku, cl::ptSubject, true);
+  clipper.AddPath(path, cl::ptClip, true);
+  cl::Paths next_waku;
+  clipper.Execute(cl::ctDifference, next_waku, cl::pftNonZero, cl::pftNonZero);
+  // 穴が空いたり、2つ以上に分割したりしたらdameフラグを立てる
+  bool dame = false;
+  if (next_waku.size() > 1) {
+    std::sort(next_waku.begin(), next_waku.end(), [](const auto& l, const auto& r) {
+      return std::abs(cl::Area(l)) > std::abs(cl::Area(r));
+    });
+    // 誤差を考え、面積15以下は許容する。
+    dame |= std::abs(cl::Area(next_waku[1])) > 15;
+    next_waku.erase(next_waku.begin() + 1, next_waku.end());
+  }
+  return std::pair<bool, cl::Paths>(dame, next_waku);
+}
+
+static std::pair<bool, cl::Paths> patch_uni(const cl::Paths& uni, const cl::Path& path)
+{
+  // uni に path を追加する
+  cl::Clipper unioner;
+  unioner.AddPaths(uni, cl::ptSubject, true);
+  unioner.AddPath(path, cl::ptClip, true);
+  cl::Paths next_uni;
+  unioner.Execute(cl::ctUnion, next_uni, cl::pftNonZero, cl::pftNonZero);
+  // 穴が空いたり、2つ以上の島ができたりしたらdameフラグを立てる
+  bool dame = false;
+  if (next_uni.size() > 1) {
+    std::sort(next_uni.begin(), next_uni.end(), [](const auto& l, const auto& r) {
+      return std::abs(cl::Area(l)) > std::abs(cl::Area(r));
+    });
+    // 誤差を考え、面積15以下は許容する。
+    dame |= std::abs(cl::Area(next_uni[1])) > 15;
+    next_uni.erase(next_uni.begin() + 1, next_uni.end());
+  }
+  // 一辺の長さが4グリッド弱以下ならdameフラグ
+  if (next_uni.size() == 1) {
+    auto v = next_uni.front();
+    int n = v.size();
+    for (int i = 0; i < n; ++i) {
+      dame |= [](const cl::IntPoint& a, const cl::IntPoint& b) {
+        return std::sqrt(std::pow(b.X - a.X, 2) + std::pow(b.Y - a.Y, 2));
+      }(v[i], v[(i + 1) % n]) < 3.5;
+    }
+  }
+  // 共通部分が大きかったらdameフラグを立てる
+  cl::Clipper intersecter;
+  intersecter.AddPaths(uni, cl::ptSubject, true);
+  intersecter.AddPath(path, cl::ptClip, true);
+  cl::Paths dst;
+  intersecter.Execute(cl::ctIntersection, dst, cl::pftNonZero, cl::pftNonZero);
+  for (const auto& v : dst) {
+    // 誤差を考え、面積15以下は許容する。
+    dame |= std::abs(cl::Area(v)) > 15;
+  }
+  return std::pair<bool, cl::Paths>(dame, next_uni);
+}
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+
+union Color {
+  unsigned int raw;
+  unsigned char bytes[4];
+};
+
+static cv::Scalar uint2scalar(unsigned int _color)
+{
+  Color color = Color{ _color };
+  return cv::Scalar(color.bytes[0], color.bytes[1], color.bytes[2], color.bytes[3]);
+}
+
+static void DrawPolygons(const cl::Paths& _paths, unsigned int fill_color, unsigned int line_color)
+{
+  cv::Mat img = cv::Mat::zeros(cv::Size(300, 300), CV_8UC4);
+  std::vector<std::vector<cv::Point>> paths;
+  std::vector<int> npts;
+
+  img = uint2scalar(0x00FFFFFF);
+
+  for (const auto& path : _paths) {
+    std::vector<cv::Point> points;
+    for (const auto& point : path) {
+      points.push_back(cv::Point(point.X, point.Y));
+    }
+    paths.push_back(points);
+    npts.push_back(points.size());
+  }
+  std::vector<cv::Point *> raw_paths(paths.size());
+  for (int i = 0; i < paths.size(); ++i) {
+    raw_paths[i] = &paths[i][0];
+  }
+
+  cv::fillPoly(img, (const cv::Point **) &raw_paths[0], &npts[0], paths.size(), uint2scalar(fill_color));
+  cv::polylines(img, (const cv::Point **) &raw_paths[0], &npts[0], paths.size(), true, uint2scalar(line_color));
+
+  cv::imshow("clipper sample", img);
+
+  cv::waitKey(0);
+  cv::destroyAllWindows();
+}
+
+class PointVecCompare {
+public:
+  bool operator()(const std::vector<im::Point> &left, const std::vector<im::Point> &right) const {
+    if (left.size() != right.size()) {
+      return left.size() < right.size();
+    }
+    const int n = left.size();
+    for (int i = 0; i < n; ++i) {
+      if (left[i].x != right[i].x) {
+        return left[i].x < right[i].x;
+      }
+      if (left[i].y != right[i].y) {
+        return left[i].y < right[i].y;
+      }
+    }
+    return false;
+  }
+};
+
 std::vector<im::Answer> tk::matsuri_search(const im::Piece& waku, const std::vector<im::Piece>& problem, const std::vector<im::Answer>& hint)
 {
   const int n = problem.size();
@@ -147,14 +278,88 @@ std::vector<im::Answer> tk::matsuri_search(const im::Piece& waku, const std::vec
   for (const auto& piece : problem) {
     paths.push_back(piece2paths(piece).front());
   }
-  for (int i = 0; i <= n; ++i) {
-    if (stacks[i].size() == 0) {
-      continue;
-    }
-    State node = stacks[i].top();
-    stacks[i].pop();
-    for (int j = 0; j < n; ++j) {
-      // pathsを当てはめる
+  double best_score = 1 << 28;;
+  Info best_info;
+  int count = 0;
+  std::set<std::vector<im::Point>, PointVecCompare> done;
+  done.insert(atom.info.haiti);
+  for (int g = 0;; ++g) {
+    std::cerr << "---- " << g << " GENERATION ----" << std::endl;
+    for (int i = 0; i <= n; ++i) {
+      if (stacks[i].size() == 0) {
+        continue;
+      }
+      std::cerr << "[" << i << "]" << std::endl;
+      State node = stacks[i].top();
+      double score = node.waku.size() == 0 ? 0 : std::abs(cl::Area(node.waku.front()));
+      if (best_score > score) {
+        best_score = score;
+        best_info = node.info;
+        ++count;
+        std::cerr << "(" << count << ") best score is renewed! : " << best_score << " at " << i << std::endl;
+        if (count % 10 == 0) {
+          DrawPolygons(node.waku, 0x160000FF, 0x600000FF); //blue
+          DrawPolygons(node.uni, 0x20FFFF00, 0x30FF0000); //orange
+        }
+      }
+      stacks[i].pop();
+      for (int j = 0; j < n; ++j) {
+        //std::cerr << "\tusing " << j << "-th path" << std::endl;
+        if (node.info.set[j]) {
+          //std::cerr << "\t\tused, skip" << std::endl;
+          continue;
+        }
+        auto next_set = node.info.set;
+        next_set[j] = true;
+        int awawa = 0;
+        // pathsを当てはめる
+        for (int x = 0; x < YOKO; ++x) {
+          for (int y = 0; y < TATE; ++y) {
+            const auto& path = paths[j];
+            // はみ出しがある配置ならNG
+            bool ok = true;
+            for (const auto& point : path) {
+              ok &= 0 <= x + point.X && x + point.X < YOKO
+                && 0 <= y + point.Y && y + point.Y < TATE;
+            }
+            if (!ok) {
+              continue;
+            }
+            cl::Path zurasied_path = path;
+            for (auto& point : zurasied_path) {
+              point.X += x;
+              point.Y += y;
+            }
+            bool dame = false;
+            auto ret_waku = cut_waku(node.waku, zurasied_path);
+            dame |= ret_waku.first;
+            auto ret_uni = patch_uni(node.uni, zurasied_path);
+            dame |= ret_uni.first;
+            if (dame) {
+              continue;
+            }
+            auto next_waku = ret_waku.second;
+            auto next_uni = ret_uni.second;
+            auto next_haiti = node.info.haiti;
+            next_haiti[j] = im::Point(x, y);
+            Info next_info(next_set, next_haiti);
+            State next(next_waku, next_uni, next_info);
+            if (done.count(next_haiti) > 0) {
+              continue;
+            }
+            done.insert(next_haiti);
+            stacks[i + 1].push(next);
+            //++awawa;
+            //cl::Paths tmp;
+            //tmp << zurasied_path;
+            //std::cerr << "zurashi" << x << ", " << y << std::endl;
+            //DrawPolygons(node.waku, 0x160000FF, 0x600000FF); //blue
+            //DrawPolygons(tmp, 0x20FFFF00, 0x30FF0000); //orange
+            //DrawPolygons(next_waku, 0x3000FF00, 0xFF006600); //solution shaded green
+          }
+        }
+        //std::cerr << "\t\t" << awawa << " transed to next" << std::endl;
+      }
     }
   }
   return std::vector<im::Answer>();
